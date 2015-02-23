49e78f9cb7be91e6331ebcc897f0004e2354e1ca
James W <jameswalsh5683@gmail.com>
2015-02-23 15:15:39 +0000

    grabbing of IP and auto copy on click

--- EzIP/Form1.Designer.cs
+++ EzIP/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolLbl = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstBox
+            // 
+            this.lstBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Items.AddRange(new object[] {
+            "The IPs of all of your adapters",
+            "Click on one to copy to clipboard",
+            "---------------------------------------------------------------------------------" +
+                "--"});
+            this.lstBox.Location = new System.Drawing.Point(0, 0);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(256, 238);
+            this.lstBox.TabIndex = 0;
+            this.lstBox.SelectedIndexChanged += new System.EventHandler(this.lstBox_SelectedIndexChanged);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolLbl});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 238);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(256, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolLbl
+            // 
+            this.toolLbl.Name = "toolLbl";
+            this.toolLbl.Size = new System.Drawing.Size(0, 22);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(256, 263);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.lstBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "EzIP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolLbl;
     }
 }
 
--- EzIP/Form1.cs
+++ EzIP/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
 
 namespace EzIP
 {
@@ -15,6 +16,56 @@ namespace EzIP
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolLbl.Text = "Click on an IP to copy to clipboard";
+            init();
+        }
+        private void init()
+        {
+            lstBox.Items.Add("The IPs of all of your adapters");
+            lstBox.Items.Add("Click on one to copy to clipboard");
+            lstBox.Items.Add("-----------------------------------------------------------------------------------");
+
+            runIPGrab();
+        }
+        private void runIPGrab()
+        {
+
+            try
+            {
+                lstBox.Items.Add("Hostname: " + Dns.GetHostName());
+                lstBox.Items.Add("-----------------------------------------------------------------------------------");
+
+                string ip;
+                IPHostEntry host = default(IPHostEntry);
+                string hostname;
+                hostname = System.Environment.MachineName;
+                host = Dns.GetHostEntry(hostname);
+                foreach (IPAddress IP in host.AddressList)
+                {
+                    if (IP.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        ip = Convert.ToString(IP);
+                        lstBox.Items.Add(ip);
+                        lstBox.Items.Add("-----------------------------------------------------------------------------------");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        private void lstBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Convert.ToString(lstBox.SelectedItem));
+            toolLbl.Text = ("Copied " + lstBox.SelectedItem + " to clipboard");
         }
     }
 }
